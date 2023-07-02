6f93a9d7697d0972c9383a15dfa1480f26110f61
shubham <shubham@shubh123.com>
2023-07-02 15:13:45 +0530

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -40,7 +40,7 @@
 import React from "react";
 const Card=(props)=>{
    return( <div class="card m-2" style={{"width": "15rem"}}>
-   <img src={props.data3.img} class="card-img-top " style={{height:"200px",objectFit:"fill"}} alt="..."/>
+   <img src={props.data3.img} class="card-img-top " style={{height:"120px",objectFit:"fill"}} alt="..."/>
    <div class="card-body">
      <p class="card-text">{props.data3.name} </p>
      <div className="container w-100"><select className="m-2  h-100 bg-gradient rounded-3" name="list" id="">{Array.from(Array(6),(e,i)=>{
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,22 +4,22 @@
 // export default function Navbar() {
 //     return (
 //         <div>
-//             <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-//                 <div className="container-fluid">
-//                     <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
-//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//                         <span className="navbar-toggler-icon"></span>
+//             <nav className Name="navbar navbar-expand-lg navbar-dark bg-success">
+//                 <div className Name="container-fluid">
+//                     <Link className Name="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+//                     <button className Name="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                         <span className Name="navbar-toggler-icon"></span>
 //                     </button>
-//                     <div className="collapse navbar-collapse" id="navbarNav">
-//                         <ul className="navbar-nav">
-//                             <li className="nav-item">
-//                                 <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+//                     <div className Name="collapse navbar-collapse" id="navbarNav">
+//                         <ul className Name="navbar-nav">
+//                             <li className Name="nav-item">
+//                                 <Link className Name="nav-link active" aria-current="page" href="/">Home</Link>
 //                             </li>
-//                             <li className="nav-item">
-//                                 <Link className="nav-link" href="/login">Login</Link>
+//                             <li className Name="nav-item">
+//                                 <Link className Name="nav-link" href="/login">Login</Link>
 //                             </li>
-//                             <li className="nav-item">
-//                                 <Link className="nav-link" href="/login">Signup</Link>
+//                             <li className Name="nav-item">
+//                                 <Link className Name="nav-link" href="/login">Signup</Link>
 //                             </li>
 //                         </ul>
 //                     </div>
@@ -27,18 +27,18 @@
 //             </nav>
 //         </div>
 
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//   <div className="container-fluid">
-//     <a className="navbar-brand" href="#">Navbar</a>
-//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon"></span>
+//         <nav className Name="navbar navbar-expand-lg navbar-light bg-light">
+//   <div className Name="container-fluid">
+//     <a className Name="navbar-brand" href="#">Navbar</a>
+//     <button className Name="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+//       <span className Name="navbar-toggler-icon"></span>
 //     </button>
-//     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-//       <div className="navbar-nav">
-//         <a className="nav-link active" aria-current="page" href="#">Home</a>
-//         <a className="nav-link" href="#">Features</a>
-//         <a className="nav-link" href="#">Pricing</a>
-//         <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//     <div className Name="collapse navbar-collapse" id="navbarNavAltMarkup">
+//       <div className Name="navbar-nav">
+//         <a className Name="nav-link active" aria-current="page" href="#">Home</a>
+//         <a className Name="nav-link" href="#">Features</a>
+//         <a className Name="nav-link" href="#">Pricing</a>
+//         <a className Name="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
 //       </div>
 //     </div>
 //   </div>
@@ -48,49 +48,56 @@
 // ------------------------------ALI MALIK CODE-----------------
 
 import React from "react";
-import Footer from "./Footer";
-import {Link, useNavigate} from "react-router-dom"
-const Navbar=()=>{
-  const navigate=useNavigate()
-  const logout=()=>{
-        localStorage.removeItem("token")
-        navigate('/login')
+// import Footer from "./Footer";
+import { Link, useNavigate } from "react-router-dom"
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("authToken")
+    navigate('/login')
   }
-    return(<>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <Link class="navbar-brand fs-1" to="/sfae">Food order</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto me-auto mb-2 mb-lg-0">
-       
-        <li class="nav-item">
-          <Link class="nav-link active fs-5" aria-current="page" to="/">Home</Link></li>
-         { localStorage.getItem("token")?
-        <div>
-        <Link class="nav-link active fs-5" aria-current="page" to="/">my orders</Link></div>:<></>}
-        
-      
-        
-        </ul>
-        {
-       
-        localStorage.getItem("token")?
-        <div>
-        
-        <div className="btn btn-success mx-1">Cart</div><div className="btn btn-success mx-1" onClick={logout}>logout</div></div>
-        :
-        <div className="d-flex">
-         <Link class="btn bg-white mx-1" to="/login">Login</Link>
-          <Link class="btn bg-white mx-1" to='/signup'  >signup</Link>
+  return (<>
+    <nav className="navbar navbar-expand-lg navbar-green bg-green">
+      <div className="container-fluid">
+        <Link className="navbar-brand fs-1" to="/sfae">Go Foodie</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto me-auto mb-2 mb-lg-0">
+
+            <li className="nav-item">
+              <Link className="btn btn-success mx-1" aria-current="page" to="/">Home</Link>
+            </li>
+            {localStorage.getItem("authToken") ?
+
+              <li className="nav-item">
+                <Link className="btn btn-success mx-1" aria-current="page" to="/">my orders</Link></li>
+              : ""}
+          </ul>
+          {
+
+            (!localStorage.getItem("authToken")) ?
+              <div className="d-flex">
+                <Link className="btn bg-white mx-1" to="/login">Login</Link>
+                <Link className="btn bg-white mx-1" to='/signup'  >signup</Link>
+              </div>
+              :
+              <div>
+                <div className="btn btn-success mx-1">My Cart</div>
+                <div className="btn btn-danger mx-1" onClick={logout}>logout</div>
+              </div>
+
+
+
+
+          }
+          
         </div>
-       
-        }
-    </div>
-  </div>
-</nav>
-    </>)
+      </div>
+    </nav>
+  </>)
 }
+
 export default Navbar
